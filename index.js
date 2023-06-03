5837bf2f45a620ef3a97dc152295af6bce7c034f
Apoll0nsLyre <gougeenzo@gmail.com>
2023-06-03 22:54:15 +0200

    change the front visual

---
+++ index.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector('.navbar');
+const contact = document.querySelector('.contact');
+
+contact.addEventListener('click', () => {
+    menu.classList.toggle('navbar');
+});
+    
